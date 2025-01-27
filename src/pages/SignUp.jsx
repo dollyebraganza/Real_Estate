f49f63dd557aa92b4923fa4aa0295829327bb14e
Dolly Ebraganza <dollyebraganza@gmail.com>
2025-01-27 16:16:42 +0530

    complete sign in page UI

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData]= useState({})
+  const handleChange = (e) => {
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]: e.target.value,
+      }
+    );
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup', formData);
+  }
+  console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className= 'text-3xl text-center font-semibold my-7'>
         SignUp</h1>
         <form className='flex flex-col gap-4 '>
         <input type="text" placeholder='username'
-          className='border p-3 rounded-lg' id='username'/>
+          className='border p-3 rounded-lg' id='username'
+          onChange={handleChange}/>
         <input type="email" placeholder='email'
           className='border p-3 rounded-lg' id='email'/>
         <input type="password" placeholder='password'
