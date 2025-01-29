abd8d7c32d930efc27a4d989a08733ffbc9ce132
Manishsingh1110 <manish03022003@gmail.com>
2025-01-29 15:26:30 +0530

    test

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -19,7 +19,7 @@ export default function SignUp() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className= 'text-3xl text-center font-semibold my-7'>
-        SignUp</h1>
+        Login</h1>
         <form className='flex flex-col gap-4 '>
         <input type="text" placeholder='username'
           className='border p-3 rounded-lg' id='username'
