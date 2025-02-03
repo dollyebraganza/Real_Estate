2c908c2c66c77ea27aa6d18bdc8c27be1f1c9689
Dolly Ebraganza <dollyebraganza@gmail.com>
2025-02-03 19:54:34 +0530

    Add Google OAuth functionality

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDR1_fFHb16jbyqyEpK5h6m7qHs9enNNIE",
+  authDomain: "real-estate-90c26.firebaseapp.com",
+  projectId: "real-estate-90c26",
+  storageBucket: "real-estate-90c26.firebasestorage.app",
+  messagingSenderId: "497956917434",
+  appId: "1:497956917434:web:712ca978fde2fd7a1cf283"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector} from 'react-redux';
 import { logInStart, logInSuccess, logInFailure} from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 export default function LogIn() {
   const [formData, setFormData]= useState({});
@@ -42,6 +43,7 @@ export default function LogIn() {
         <input type="password" placeholder='password'
           className='border p-3 rounded-lg' id='password'/>
           <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Log in</button>
+          <OAuth />
         </form>
         <div className="flex gap-2 mt-5">
           <p>Dont have an account?</p>
